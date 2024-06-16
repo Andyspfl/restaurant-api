1cfafb4cc742fee9db9159a1f7e7224e65d583bc
Andy Mayta <123778354+Andyspfl@users.noreply.github.com>
2024-06-16 07:13:27 +0000

    update restaurant-api

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt_identity
 from werkzeug.security import check_password_hash
 
 from app.models.user_model import User
